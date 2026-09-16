11940ac93b7a0ea63534fc1080f0ca96822b20be
anubissss1 <157205073+anubissss1@users.noreply.github.com>
2026-09-16 18:17:02 +0500

    Add files via upload

---
+++ assignmentWeek1.swift
@@ -0,0 +1,42 @@
+import Playgrounds
+
+let firstName = "Dimash"
+let lastName = "Abdykadyr"
+
+let birthYear = 2004
+let currentYear = 2026
+
+let age = currentYear - birthYear
+let isStudent = true
+let height = 1.87
+
+let country = "Kazakhstan"
+let favoriteSubject = "Software Engineering"
+
+let hobby = "Gaming"
+let numberOfHobbies = 4
+let favoriteNumber = 67
+let isHobbyCreative = false
+
+// Additional information
+
+let favoriteGame = "Valorant"
+let programmingLanguage = "Swift"
+
+let futureGoals = "I want to become a professional IOS developer"
+let emoji = "💻"
+
+let lifeStory = """
+My name is \(firstName) \(lastName).
+I am \(age) years old and I was born in \(birthYear).
+I am currently a student: \(isStudent).
+I am \(height) meters tall and I live in \(country).
+My favorite subject is \(favoriteSubject).
+I enjoy \(hobby), especially \(favoriteGame).
+I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber).
+My hobby is creative: \(isHobbyCreative).
+I am interested in \(programmingLanguage).
+In the future, \(futureGoals) \(emoji)
+"""
+
+print(lifeStory)
